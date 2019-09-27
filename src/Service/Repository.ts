75ecc1e84773a68e9a86eb3862f9b7a195f17bfd
Soulike <316413310@qq.com>
2019-09-27 13:23:32 +0800

    完成 Service 层方法，调整文档
    - Repository 模块下的 delete 路由调整为 del，以规避关键字冲突
    - 添加删除文件/文件夹的方法 rm
    - 完成 Repository 模块下的 del Service 方法

--- src/Service/Repository.ts
+++ src/Service/Repository.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {GIT, SERVER} from '../CONFIG';
 import {promises as fsPromise} from 'fs';
 import {spawn} from 'child_process';
+import {File} from '../Function';
 
 export namespace Repository
 {
@@ -50,22 +51,7 @@ export namespace Repository
             {
                 try
                 {
-                    return new Promise((resolve, reject) =>
-                    {
-                        // 截止到 Node 12，fs 中的文件夹递归删除仍然是实验性的，因此在此直接调用系统的 rm。为了跨平台考虑，可等 Api 稳定后后修改
-                        const childProcess = spawn(`rm -rf ${repoPath}`, {
-                            shell: true,
-                        });
-                        childProcess.on('exit', () =>
-                        {
-                            resolve();
-                        });
-
-                        childProcess.on('error', err =>
-                        {
-                            reject(err);
-                        });
-                    });
+                    await File.rm(repoPath);
                 }
                 catch (e)
                 {
@@ -76,6 +62,50 @@ export namespace Repository
 
             throw e;    // 把错误抛到外层
         }
+
+        return new ServiceResponse<void>(200, {}, new ResponseBody<void>(true));
+    }
+
+    export async function del(username: RepositoryClass['username'], name: RepositoryClass['name']): Promise<ServiceResponse<void>>
+    {
+        // 检查仓库是否存在
+        if ((await RepositoryTable.select(username, name)) === null)
+        {
+            return new ServiceResponse<void>(404, {}, new ResponseBody<void>(false, '仓库不存在'));
+        }
+        const repoPath = path.join(GIT.ROOT, username, `${name}.git`);
+        /*
+        * 删除采用以下步骤：
+        * 1. 创建一个临时文件夹
+        * 2. 将仓库文件夹移动到临时文件夹
+        * 3. 删除数据库
+        * 4. 删除临时文件夹中的仓库文件夹
+        * 如果以上有任意一步失败都将仓库文件夹移动回原位。数据库出错可以自己回滚
+        * */
+
+        // 创建临时文件夹并移动仓库文件夹
+        const tempPath = await fsPromise.mkdtemp('repo-');
+        try
+        {
+            await fsPromise.rename(repoPath, tempPath);
+        }
+        catch (e)   // 如果这一步就失败了，就直接放弃操作
+        {
+            throw e;
+        }
+
+        // 文件夹移动成功，删除数据库记录
+        try
+        {
+            await RepositoryTable.del(username, name);
+        }
+        catch (e)   // 数据库记录删除失败，把仓库文件夹移动回去
+        {
+            await fsPromise.rename(tempPath, repoPath);
+            throw e;
+        }
+        // 数据库记录删除成功，删除临时文件夹
+        await File.rm(tempPath);
 
         return new ServiceResponse<void>(200, {}, new ResponseBody<void>(true));
     }
