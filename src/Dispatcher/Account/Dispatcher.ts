4eb6753491caf4c60cd41cbfcf746a65598e4cf3
Soulike <316413310@qq.com>
2019-09-27 10:55:42 +0800

    完成 Account Dispatcher 层 /login

--- src/Dispatcher/Account/Dispatcher.ts
+++ src/Dispatcher/Account/Dispatcher.ts
@@ -1,9 +1,10 @@
 import Router from '@koa/router';
-import {LOGIN} from './ROUTE';
+import {LOGIN, REGISTER} from './ROUTE';
 import koaBody from 'koa-body';
 import {BODY} from '../../CONFIG';
 import {ResponseBody} from '../../Class';
 import {Account as AccountService} from '../../Service';
+import validator from 'validator';
 
 export const dispatcher = (router: Router) =>
 {
@@ -20,6 +21,34 @@ export const dispatcher = (router: Router) =>
             else
             {
                 const {statusCode, headers, body} = await AccountService.login(username, hash);
+                if (headers)
+                {
+                    ctx.response.set(headers);
+                }
+                ctx.response.body = body;
+                ctx.response.status = statusCode;
+            }
+        }
+        finally
+        {
+            await next();
+        }
+    });
+
+    router.post(REGISTER, koaBody(BODY), async (ctx, next) =>
+    {
+        try
+        {
+            const {username, hash, email} = ctx.request.body;
+            if (typeof username !== 'string' || typeof hash !== 'string' || hash.length !== 64 ||
+                typeof email !== 'string' || !validator.isEmail(email))
+            {
+                ctx.response.status = 400;
+                ctx.response.body = new ResponseBody<void>(false, '请求参数错误');
+            }
+            else
+            {
+                const {statusCode, headers, body} = await AccountService.register(username, hash, email);
                 if (headers)
                 {
                     ctx.response.set(headers);
