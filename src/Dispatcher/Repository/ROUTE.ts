afb861af659658f71eddc33780ebb2dd177e7093
Soulike <316413310@qq.com>
2019-09-28 15:00:02 +0800

    Repository 模块添加接口
    - Repository 模块添加 /getPublicList、/getPersonalList 接口
    - 对应接口添加文档

--- src/Dispatcher/Repository/ROUTE.ts
+++ src/Dispatcher/Repository/ROUTE.ts
@@ -6,3 +6,5 @@ function prefix(url: string): string
 export const CREATE = prefix('/create');
 export const DEL = prefix('/del');
 export const GET_FILE = prefix('/getFile');
+export const GET_PUBLIC_LIST = prefix('/getPublicList');
+export const GET_PERSONAL_LIST = prefix('/getPersonalList');
