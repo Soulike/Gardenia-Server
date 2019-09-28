fa6c7bee41ce07d11293e4df61faf55c6f8d26ea
Soulike <316413310@qq.com>
2019-09-28 20:30:33 +0800

    修改 RequestLogger 中间件

--- src/Middleware/RequestLogger.ts
+++ src/Middleware/RequestLogger.ts
@@ -5,12 +5,11 @@ export const requestLogger = (): Koa.Middleware =>
 {
     return async (ctx, next) =>
     {
-        const {method, path, query, body} = ctx.request;
-        const session = ctx.session;
-        signale.info(`${method} request from session ${JSON.stringify(session)} to ${path} with query ${JSON.stringify(query)} and body ${JSON.stringify(body)}`);
+        const {method, path, query, ip} = ctx.request;
+        signale.info(`${method} request from ${ip} to ${path} with query ${JSON.stringify(query)}`);
         await next();
         const {status, message, body: resBody} = ctx.response;
-        signale.info(`Response to session ${JSON.stringify(session)} with status ${status} ${message} and body ${JSON.stringify(resBody,
+        signale.info(`Response to ${ip} with status ${status} ${message} and body ${JSON.stringify(resBody,
             ((_key, value) =>
             {
                 if (typeof value === 'string' && value.length > 100)
