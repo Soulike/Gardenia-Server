cc3e962db9f3bcd0b6e9ec1f79470ba3c09c6c58
Soulike <316413310@qq.com>
2019-10-02 15:19:59 +0800

    fileInfo 接口改进与 bug 修复
    - 将文件哈希与类型获取独立成函数
    - 修复命令未写 cwd 的 bug
    - 补充文档疏漏

--- src/Function/Git.ts
+++ src/Function/Git.ts
@@ -77,3 +77,39 @@ export async function getFileCommitInfoList(repositoryPath: string, branch: stri
     }));
     return fileCommitInfoList;
 }
+
+/**
+ * @description 获取对象的哈希
+ * */
+export async function getObjectHash(repositoryPath: string, filePath: string, commitHash: string): Promise<string | null>
+{
+    // 格式为 "100644 blob bbdf566e2f8da7288558241c5ffba6c32f943826	yarn.lock"
+    const lsTreeOut = await execPromise(`git ls-tree ${commitHash} -- ${filePath}`,
+        {cwd: repositoryPath}) as string;
+    if (lsTreeOut.length === 0) // 没有输出则文件不存在，返回 null
+    {
+        return null;
+    }
+    else
+    {
+        return lsTreeOut.split(/\s+/)[2];
+    }
+}
+
+/**
+ * @description 获取对象的类型
+ * */
+export async function getObjectType(repositoryPath: string, filePath: string, commitHash: string): Promise<ObjectType | null>
+{
+    // 格式为 "100644 blob bbdf566e2f8da7288558241c5ffba6c32f943826	yarn.lock"
+    const lsTreeOut = await execPromise(`git ls-tree ${commitHash} -- ${filePath}`,
+        {cwd: repositoryPath}) as string;
+    if (lsTreeOut.length === 0) // 没有输出则文件不存在，返回 null
+    {
+        return null;
+    }
+    else
+    {
+        return lsTreeOut.split(/\s+/)[1] === 'tree' ? ObjectType.TREE : ObjectType.BLOB;
+    }
+}
--- src/Service/RepositoryInfo.ts
+++ src/Service/RepositoryInfo.ts
@@ -174,55 +174,51 @@ export async function fileInfo(username: string, repositoryName: string, filePat
             new ResponseBody<void>(false, '仓库不存在'));
     }
     const repoPath = path.join(GIT.ROOT, username, `${repositoryName}.git`);
-    let lsTreeOut = '';
+    let objectHash: string | null = null;
+    let objectType: ObjectType | null = null;
     try
     {
-        // 根据提交哈希和文件名，获取对象哈希
-        lsTreeOut = await Promisify.execPromise(`git ls-tree ${commitHash} -- ${filePath}`,
-            {cwd: repoPath}) as string;
+        // 获取对象哈希和类型
+        const result = await Promise.all([
+            Git.getObjectHash(repoPath, filePath, commitHash),
+            Git.getObjectType(repoPath, filePath, commitHash),
+        ]);
+        objectHash = result[0];
+        objectType = result[1];
     }
     catch (e)   // 当提交 hash 不存在时会有 fatal: not a tree object
     {
         return new ServiceResponse<void>(404, {},
             new ResponseBody<void>(false, '提交不存在'));
     }
-    if (lsTreeOut.length === 0)  // 没有输出，那么文件不存在
+    if (objectHash === null || objectType === null)
+    {
+        return new ServiceResponse<void>(404, {},
+            new ResponseBody<void>(false, '文件不存在'));
+    }
+
+    // 把文件内容送给 file 命令行工具查看类型
+    const fileOut = await Promisify.execPromise(`git cat-file -p ${objectHash} | file -`,
+        {cwd: repoPath}) as string;
+    if (fileOut.toLowerCase().includes('text')) // 当 file 工具的输出包含 "text" 时，是文本文件
+    {
+        // 获取文件大小
+        const sizeString = await Promisify.execPromise(`git cat-file -s ${objectHash}`, {cwd: repoPath}) as string;
+        const size = Number.parseInt(sizeString);
+        return new ServiceResponse<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(200, {},
+            new ResponseBody<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(
+                true, '', {
+                    exists: true, isBinary: false, type: objectType, size,
+                },
+            ));
+    }
+    else    // 是二进制文件
     {
         return new ServiceResponse<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(200, {},
-            new ResponseBody<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(true, '', {exists: false}));
-    }
-    else    // 有输出，格式为 "100644 blob bbdf566e2f8da7288558241c5ffba6c32f943826	yarn.lock"
-    {
-        // 分割输出
-        const lsTreeOutSplit = lsTreeOut.split(/\s+/);
-        // 提取第二部分文件类型
-        const typeString = lsTreeOutSplit[1];
-        // 提取第三部分文件对象哈希
-        const objectHash = lsTreeOutSplit[2];
-        // 把文件内容送给 file 命令行工具查看类型
-        const fileOut = await Promisify.execPromise(`git cat-file -p ${objectHash} | file -`,
-            {cwd: repoPath}) as string;
-        if (fileOut.toLowerCase().includes('text')) // 当 file 工具的输出包含 "text" 时，是文本文件
-        {
-            // 获取文件大小
-            const sizeString = await Promisify.execPromise(`git cat-file -s ${objectHash}`) as string;
-            const type = typeString === 'blob' ? ObjectType.BLOB : ObjectType.TREE;
-            const size = Number.parseInt(sizeString);
-            return new ServiceResponse<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(200, {},
-                new ResponseBody<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(
-                    true, '', {
-                        exists: true, isBinary: false, type, size,
-                    },
-                ));
-        }
-        else    // 是二进制文件
-        {
-            return new ServiceResponse<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(200, {},
-                new ResponseBody<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(
-                    true, '', {
-                        exists: true, isBinary: true,
-                    },
-                ));
-        }
+            new ResponseBody<{ exists: boolean, type?: ObjectType, size?: number, isBinary?: boolean }>(
+                true, '', {
+                    exists: true, isBinary: true,
+                },
+            ));
     }
 }
