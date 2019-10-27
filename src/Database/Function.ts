b736a1afa2070499f060dfb16ea61ccb9f760c6a
Soulike <316413310@qq.com>
2019-10-27 19:04:31 +0800

    改进 executeTransaction 工具函数
    - executeTransaction 现在可以返回 SQL 返回的值

--- src/Database/Function.ts
+++ src/Database/Function.ts
@@ -5,13 +5,14 @@ import {Client, PoolClient} from 'pg';
  * @param client - pg 创建的 Client 实例。根据文档，此处不能是 Pool
  * @param transaction - 要执行的事务
  * */
-export async function executeTransaction<T extends Client | PoolClient>(client: T, transaction: (client: T) => Promise<any>)
+export async function executeTransaction<T extends Client | PoolClient, R>(client: T, transaction: (client: T) => Promise<R>): Promise<R>
 {
     try
     {
         await client.query('START TRANSACTION');
-        await transaction(client);    // 传入参数保证事务使用的是同一个 Client 实例
+        const queryResult = await transaction(client);    // 传入参数保证事务使用的是同一个 Client 实例
         await client.query('COMMIT');
+        return queryResult;
     }
     catch (e)
     {
