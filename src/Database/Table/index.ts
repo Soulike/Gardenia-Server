a22831024ff69edb81e5b4286362c57e20023cb0
Soulike <316413310@qq.com>
2019-10-29 12:54:32 +0800

    修复 bug
    - 修复 Group 表未导出的 bug

--- src/Database/Table/index.ts
+++ src/Database/Table/index.ts
@@ -1,5 +1,6 @@
 import * as Account from './Account';
 import * as Profile from './Profile';
 import * as Repository from './Repository';
+import * as Group from './Group';
 
-export {Account, Profile, Repository};
+export {Account, Profile, Repository, Group};
