abbf2ed1bd9d02f175fab8e3f66791aab6366d32
Soulike <316413310@qq.com>
2019-11-18 21:41:58 +0800

    优化测试代码
    - 减少 Profile 服务层测试代码冗余

--- src/Service/__tests__/Profile.ts
+++ src/Service/__tests__/Profile.ts
@@ -8,95 +8,84 @@ import path from 'path';
 import os from 'os';
 import imageminWebp from 'imagemin-webp';
 import {SERVER} from '../../CONFIG';
+import {Profile as ProfileTable} from '../../Database';
 
 const fakeProfile = new Profile(faker.random.word(), faker.name.firstName(), faker.internet.email(), '');
+
+const databaseMock = {
+    Profile: {
+        selectByUsername: jest.fn<ReturnType<typeof ProfileTable.selectByUsername>,
+            Parameters<typeof ProfileTable.selectByUsername>>(),
+        update: jest.fn<ReturnType<typeof ProfileTable.update>,
+            Parameters<typeof ProfileTable.update>>().mockResolvedValue(undefined),
+    },
+};
+
+const fseMock = {
+    move: jest.fn().mockResolvedValue(undefined),
+    remove: jest.fn().mockResolvedValue(undefined),
+};
+const imageminMock = jest.fn().mockResolvedValue(undefined);
 
 describe(get, () =>
 {
     beforeEach(() =>
     {
         jest.resetModules();
+        jest.resetAllMocks();
+        jest.mock('../../Database', () => databaseMock);
     });
 
     it('should check session', async function ()
     {
-        const mockObject = {
-            Profile: {
-                selectByUsername: jest.fn().mockResolvedValue(fakeProfile),
-            },
-        };
-        jest.mock('../../Database', () => mockObject);
+        databaseMock.Profile.selectByUsername.mockResolvedValue(fakeProfile);
         const {get} = await import('../Profile');
-        expect(get({} as unknown as Session)).rejects.toEqual(new InvalidSessionError());
-        expect(mockObject.Profile.selectByUsername.mock.calls.length).toBe(0);
+        await expect(get({} as unknown as Session)).rejects.toEqual(new InvalidSessionError());
+        expect(databaseMock.Profile.selectByUsername.mock.calls.length).toBe(0);
     });
 
     it('should get profile by session', async function ()
     {
-        const mockObject = {
-            Profile: {
-                selectByUsername: jest.fn().mockResolvedValue(fakeProfile),
-            },
-        };
-        jest.mock('../../Database', () => mockObject);
+        databaseMock.Profile.selectByUsername.mockResolvedValue(fakeProfile);
         const {get} = await import('../Profile');
         const response = await get({username: fakeProfile.username} as unknown as Session);
         expect(response).toEqual(new ServiceResponse<ProfileClass>(200, {},
             new ResponseBody<ProfileClass>(true, '', fakeProfile)));
-        expect(mockObject.Profile.selectByUsername.mock.calls.length).toBe(1);
-        expect(mockObject.Profile.selectByUsername.mock.calls[0][0]).toBe(fakeProfile.username);
+        expect(databaseMock.Profile.selectByUsername.mock.calls.pop()).toEqual([fakeProfile.username]);
     });
 
     it('should get profile by account', async function ()
     {
-        const mockObject = {
-            Profile: {
-                selectByUsername: jest.fn().mockResolvedValue(fakeProfile),
-            },
-        };
-        jest.mock('../../Database', () => mockObject);
+        databaseMock.Profile.selectByUsername.mockResolvedValue(fakeProfile);
         const {get} = await import('../Profile');
         const response = await get({} as unknown as Session, {username: fakeProfile.username});
         expect(response).toEqual(new ServiceResponse<ProfileClass>(200, {},
             new ResponseBody<ProfileClass>(true, '', fakeProfile)));
-        expect(mockObject.Profile.selectByUsername.mock.calls.length).toBe(1);
-        expect(mockObject.Profile.selectByUsername.mock.calls[0][0]).toBe(fakeProfile.username);
+        expect(databaseMock.Profile.selectByUsername.mock.calls.pop()).toEqual([fakeProfile.username]);
     });
 
     it('should get profile by account first', async function ()
     {
-        const mockObject = {
-            Profile: {
-                selectByUsername: jest.fn().mockResolvedValue(fakeProfile),
-            },
-        };
-        jest.mock('../../Database', () => mockObject);
+        databaseMock.Profile.selectByUsername.mockResolvedValue(fakeProfile);
         const {get} = await import('../Profile');
         const response = await get(
             {username: faker.random.word()} as unknown as Session,
             {username: fakeProfile.username});
         expect(response).toEqual(new ServiceResponse<ProfileClass>(200, {},
             new ResponseBody<ProfileClass>(true, '', fakeProfile)));
-        expect(mockObject.Profile.selectByUsername.mock.calls.length).toBe(1);
-        expect(mockObject.Profile.selectByUsername.mock.calls[0][0]).toBe(fakeProfile.username);
+        expect(databaseMock.Profile.selectByUsername.mock.calls.pop()).toEqual([fakeProfile.username]);
     });
 
     it('should check account existence by account', async function ()
     {
-        const mockObject = {
-            Profile: {
-                selectByUsername: jest.fn().mockResolvedValue(null),
-            },
-        };
-        jest.mock('../../Database', () => mockObject);
+        databaseMock.Profile.selectByUsername.mockResolvedValue(null);
         const {get} = await import('../Profile');
         const response = await get(
             {} as unknown as Session,
             {username: fakeProfile.username});
         expect(response).toEqual(new ServiceResponse<ProfileClass>(404, {},
             new ResponseBody<ProfileClass>(false, '用户不存在')));
-        expect(mockObject.Profile.selectByUsername.mock.calls.length).toBe(1);
-        expect(mockObject.Profile.selectByUsername.mock.calls[0][0]).toBe(fakeProfile.username);
+        expect(databaseMock.Profile.selectByUsername.mock.calls.pop()).toEqual([fakeProfile.username]);
     });
 });
 
@@ -105,40 +94,29 @@ describe(set, () =>
     beforeEach(() =>
     {
         jest.resetModules();
+        jest.resetAllMocks();
+        jest.mock('../../Database', () => databaseMock);
     });
 
     it('should check session', async function ()
     {
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockResolvedValue(undefined),
-            },
-        };
-        jest.mock('../../Database', () => databaseMock);
         await expect(set({}, {} as unknown as Session)).rejects.toBeInstanceOf(InvalidSessionError);
         expect(databaseMock.Profile.update.mock.calls.length).toBe(0);
     });
 
     it('should set profile', async function ()
     {
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockResolvedValue(undefined),
-            },
-        };
         const fakeAccount = new Account(faker.name.firstName(), faker.random.alphaNumeric(64));
         const fakeProfile: Partial<Omit<Profile, 'avatar' | 'username'>> = {
             email: faker.internet.email(),
             nickname: faker.name.firstName(),
         };
-        jest.mock('../../Database', () => databaseMock);
         const {set} = await import('../Profile');
         expect(
             await set(fakeProfile, {username: fakeAccount.username} as unknown as Session),
         ).toEqual(new ServiceResponse(200, {},
             new ResponseBody(true)));
-        expect(databaseMock.Profile.update.mock.calls.length).toBe(1);
-        expect(databaseMock.Profile.update.mock.calls[0]).toEqual([
+        expect(databaseMock.Profile.update.mock.calls.pop()).toEqual([
             fakeProfile,
             {username: fakeAccount.username},
         ]);
@@ -150,15 +128,14 @@ describe(updateAvatar, () =>
     beforeEach(() =>
     {
         jest.resetModules();
+        jest.resetAllMocks();
+        jest.mock('../../Database', () => databaseMock);
+        jest.mock('fs-extra', () => fseMock);
+        jest.mock('imagemin', () => imageminMock);
     });
 
     it('should check session', async function ()
     {
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockResolvedValue(undefined),
-            },
-        };
         const fakeFile: File = {
             size: 0,
             path: '',
@@ -168,7 +145,6 @@ describe(updateAvatar, () =>
             hash: faker.random.alphaNumeric(64),
             toJSON: () => ({}),
         };
-        jest.mock('../../Database', () => databaseMock);
         await expect(updateAvatar(fakeFile, {} as unknown as Session)).rejects.toBeInstanceOf(InvalidSessionError);
         expect(databaseMock.Profile.update.mock.calls.length).toBe(0);
     });
@@ -179,16 +155,6 @@ describe(updateAvatar, () =>
             faker.name.firstName(),
             faker.random.alphaNumeric(64),
         );
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockResolvedValue(undefined),
-            },
-        };
-        const fseMock = {
-            move: jest.fn().mockResolvedValue(undefined),
-            remove: jest.fn().mockResolvedValue(undefined),
-        };
-        const imageminMock = jest.fn().mockResolvedValue(undefined);
         const fakeFile: File = {
             size: 0,
             path: path.join(faker.random.word(), faker.random.word(), faker.random.word()),
@@ -201,22 +167,16 @@ describe(updateAvatar, () =>
         const avatarPath = path.join(SERVER.STATIC_FILE_PATH, 'avatar', `${fakeAccount.username}.webp`);
         const tempAvatarPath = path.join(os.tmpdir(), `${fakeAccount.username}.webp`);
 
-        jest.mock('../../Database', () => databaseMock);
-        jest.mock('fs-extra', () => fseMock);
-        jest.mock('imagemin', () => imageminMock);
-
         const {updateAvatar} = await import('../Profile');
         expect(
             await updateAvatar(fakeFile, {username: fakeAccount.username} as unknown as Session),
         ).toEqual(new ServiceResponse(200, {},
             new ResponseBody(true)));
-        expect(databaseMock.Profile.update.mock.calls.length).toBe(1);
-        expect(databaseMock.Profile.update.mock.calls[0]).toEqual([
+        expect(databaseMock.Profile.update.mock.calls.pop()).toEqual([
             {avatar: `/avatar/${fakeAccount.username}.webp`}, {username: fakeAccount.username},
         ]);
 
-        expect(imageminMock.mock.calls.length).toBe(1);
-        expect(JSON.stringify(imageminMock.mock.calls[0])).toBe(JSON.stringify(
+        expect(JSON.stringify(imageminMock.mock.calls.pop())).toBe(JSON.stringify(
             [
                 [fakeFile.path],
                 {
@@ -235,8 +195,7 @@ describe(updateAvatar, () =>
             ],
         ));
 
-        expect(fseMock.move.mock.calls.length).toBe(1);
-        expect(fseMock.move.mock.calls[0]).toEqual([tempAvatarPath, avatarPath, {overwrite: true}]);
+        expect(fseMock.move.mock.calls.pop()).toEqual([tempAvatarPath, avatarPath, {overwrite: true}]);
 
         expect(fseMock.remove.mock.calls.length).toBe(2);
         expect(fseMock.remove.mock.calls).toEqual(expect.arrayContaining(
@@ -248,20 +207,11 @@ describe(updateAvatar, () =>
 
     it('should process imagemin error', async function ()
     {
+        imageminMock.mockRejectedValue(new Error());
         const fakeAccount = new Account(
             faker.name.firstName(),
             faker.random.alphaNumeric(64),
         );
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockResolvedValue(undefined),
-            },
-        };
-        const fseMock = {
-            move: jest.fn().mockResolvedValue(undefined),
-            remove: jest.fn().mockResolvedValue(undefined),
-        };
-        const imageminMock = jest.fn().mockRejectedValue(new Error());
         const fakeFile: File = {
             size: 0,
             path: path.join(faker.random.word(), faker.random.word(), faker.random.word()),
@@ -273,10 +223,6 @@ describe(updateAvatar, () =>
         };
         const tempAvatarPath = path.join(os.tmpdir(), `${fakeAccount.username}.webp`);
 
-        jest.mock('../../Database', () => databaseMock);
-        jest.mock('fs-extra', () => fseMock);
-        jest.mock('imagemin', () => imageminMock);
-
         const {updateAvatar} = await import('../Profile');
         await expect(
             updateAvatar(fakeFile, {username: fakeAccount.username} as unknown as Session),
@@ -284,8 +230,7 @@ describe(updateAvatar, () =>
 
         expect(databaseMock.Profile.update.mock.calls.length).toBe(0);
 
-        expect(imageminMock.mock.calls.length).toBe(1);
-        expect(JSON.stringify(imageminMock.mock.calls[0])).toBe(JSON.stringify(
+        expect(JSON.stringify(imageminMock.mock.calls.pop())).toBe(JSON.stringify(
             [
                 [fakeFile.path],
                 {
@@ -306,8 +251,10 @@ describe(updateAvatar, () =>
 
         expect(fseMock.move.mock.calls.length).toBe(0);
 
-        expect(fseMock.remove.mock.calls.length).toBe(2);
-        expect(fseMock.remove.mock.calls).toEqual(expect.arrayContaining(
+        expect([
+            fseMock.remove.mock.calls.pop(),
+            fseMock.remove.mock.calls.pop()],
+        ).toEqual(expect.arrayContaining(
             [
                 [fakeFile.path],
                 [tempAvatarPath],
@@ -316,20 +263,11 @@ describe(updateAvatar, () =>
 
     it('should process database error', async function ()
     {
+        databaseMock.Profile.update.mockRejectedValue(new Error());
         const fakeAccount = new Account(
             faker.name.firstName(),
             faker.random.alphaNumeric(64),
         );
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockRejectedValue(new Error()),
-            },
-        };
-        const fseMock = {
-            move: jest.fn().mockResolvedValue(undefined),
-            remove: jest.fn().mockResolvedValue(undefined),
-        };
-        const imageminMock = jest.fn().mockResolvedValue(undefined);
         const fakeFile: File = {
             size: 0,
             path: path.join(faker.random.word(), faker.random.word(), faker.random.word()),
@@ -341,23 +279,17 @@ describe(updateAvatar, () =>
         };
         const tempAvatarPath = path.join(os.tmpdir(), `${fakeAccount.username}.webp`);
 
-        jest.mock('../../Database', () => databaseMock);
-        jest.mock('fs-extra', () => fseMock);
-        jest.mock('imagemin', () => imageminMock);
-
         const {updateAvatar} = await import('../Profile');
         await expect(
             updateAvatar(fakeFile, {username: fakeAccount.username} as unknown as Session),
         ).rejects.toThrow();
 
-        expect(databaseMock.Profile.update.mock.calls.length).toBe(1);
-        expect(databaseMock.Profile.update.mock.calls[0]).toEqual([
+        expect(databaseMock.Profile.update.mock.calls.pop()).toEqual([
             {avatar: `/avatar/${fakeAccount.username}.webp`}, {username: fakeAccount.username},
         ]);
 
-        expect(imageminMock.mock.calls.length).toBe(1);
-        expect(JSON.stringify(imageminMock.mock.calls[0])).toBe(JSON.stringify(
-            [
+        expect(JSON.stringify(imageminMock.mock.calls.pop())).toEqual(
+            JSON.stringify([
                 [fakeFile.path],
                 {
                     destination: tempAvatarPath,
@@ -372,8 +304,8 @@ describe(updateAvatar, () =>
                         }),
                     ],
                 },
-            ],
-        ));
+            ]),
+        );
 
         expect(fseMock.move.mock.calls.length).toBe(0);
 
@@ -387,20 +319,11 @@ describe(updateAvatar, () =>
 
     it('should process fse move error', async function ()
     {
+        fseMock.move.mockRejectedValue(new Error());
         const fakeAccount = new Account(
             faker.name.firstName(),
             faker.random.alphaNumeric(64),
         );
-        const databaseMock = {
-            Profile: {
-                update: jest.fn().mockResolvedValue(undefined),
-            },
-        };
-        const fseMock = {
-            move: jest.fn().mockRejectedValue(new Error()),
-            remove: jest.fn().mockResolvedValue(undefined),
-        };
-        const imageminMock = jest.fn().mockResolvedValue(undefined);
         const fakeFile: File = {
             size: 0,
             path: path.join(faker.random.word(), faker.random.word(), faker.random.word()),
@@ -412,10 +335,6 @@ describe(updateAvatar, () =>
         };
         const avatarPath = path.join(SERVER.STATIC_FILE_PATH, 'avatar', `${fakeAccount.username}.webp`);
         const tempAvatarPath = path.join(os.tmpdir(), `${fakeAccount.username}.webp`);
-
-        jest.mock('../../Database', () => databaseMock);
-        jest.mock('fs-extra', () => fseMock);
-        jest.mock('imagemin', () => imageminMock);
 
         const {updateAvatar} = await import('../Profile');
         await expect(
@@ -430,9 +349,8 @@ describe(updateAvatar, () =>
             {avatar: ''}, {username: fakeAccount.username},
         ]);
 
-        expect(imageminMock.mock.calls.length).toBe(1);
-        expect(JSON.stringify(imageminMock.mock.calls[0])).toBe(JSON.stringify(
-            [
+        expect(JSON.stringify(imageminMock.mock.calls.pop())).toBe(
+            JSON.stringify([
                 [fakeFile.path],
                 {
                     destination: tempAvatarPath,
@@ -447,14 +365,15 @@ describe(updateAvatar, () =>
                         }),
                     ],
                 },
-            ],
-        ));
+            ]),
+        );
 
-        expect(fseMock.move.mock.calls.length).toBe(1);
-        expect(fseMock.move.mock.calls[0]).toEqual([tempAvatarPath, avatarPath, {overwrite: true}]);
+        expect(fseMock.move.mock.calls.pop()).toEqual([tempAvatarPath, avatarPath, {overwrite: true}]);
 
-        expect(fseMock.remove.mock.calls.length).toBe(3);
-        expect(fseMock.remove.mock.calls).toEqual(expect.arrayContaining(
+        expect([
+            fseMock.remove.mock.calls.pop(),
+            fseMock.remove.mock.calls.pop(),
+            fseMock.remove.mock.calls.pop()]).toEqual(expect.arrayContaining(
             [
                 [fakeFile.path],
                 [tempAvatarPath],
