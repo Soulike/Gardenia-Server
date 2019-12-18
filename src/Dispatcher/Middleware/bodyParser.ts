d997df8218e41ef05d917689177989d08d229cf9
Soulike <316413310@qq.com>
2019-12-18 20:29:59 +0800

    修复分发层中间件 bug，添加测试
    - 修复 bodyParser 中间件可能会把 null 赋值给 ctx.request.body 的问题
    - 添加 bodyParser 中间件的测试

--- src/Dispatcher/Middleware/bodyParser.ts
+++ src/Dispatcher/Middleware/bodyParser.ts
@@ -1,16 +1,25 @@
 import {IRouteHandler} from '../Interface';
 import koaBody, {IKoaBodyOptions} from 'koa-body';
 import {WrongParameterError} from '../Class';
+import compose from 'koa-compose';
 
-const bodyParser: IRouteHandler = () =>
-{
-    return koaBody({
+const bodyParser: IRouteHandler = () => compose([
+    // 调用 koa-body 解析请求体
+    koaBody({
         multipart: true,
         onError: () =>
         {
             throw new WrongParameterError();
         },
-    } as IKoaBodyOptions);
-};
+    } as IKoaBodyOptions),
+    async (ctx, next) =>
+    {
+        if (ctx.request.body === null)  // 如果解析结果是 null，抛出错误
+        {
+            throw new WrongParameterError();
+        }
+        await next();
+    },
+]);
 
 export default bodyParser;
