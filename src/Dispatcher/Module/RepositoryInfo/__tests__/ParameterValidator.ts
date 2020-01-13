90cc884d23000f514f3ad2020911fcb6dd20c2f6
Soulike <316413310@qq.com>
2020-01-13 16:06:51 +0800

    添加分发层测试
    - 完成 RepositoryInfo 模块分发层参数验证函数 directory、commitCount、fileInfo、rawFile 测试

--- src/Dispatcher/Module/RepositoryInfo/__tests__/ParameterValidator.ts
+++ src/Dispatcher/Module/RepositoryInfo/__tests__/ParameterValidator.ts
@@ -1,5 +1,5 @@
 import {Account, Repository} from '../../../../Class';
-import {lastCommit, repository} from '../ParameterValidator';
+import {commitCount, directory, fileInfo, lastCommit, rawFile, repository} from '../ParameterValidator';
 import 'jest-extended';
 
 describe('repository', () =>
@@ -235,5 +235,546 @@ describe('lastCommit', () =>
             filePath: 255235,
         };
         expect(lastCommit(fakeBody)).toBeFalse();
+    });
+});
+
+describe('directory', () =>
+{
+    it('should handle body without account', function ()
+    {
+        const fakeBody: {
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account (null)', function ()
+    {
+        const fakeBody: {
+            account: null,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            account: null,
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account.username (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Record<keyof Pick<Account, 'username'>, any>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            account: {username: 2552},
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body without repository', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            account: {username: '2552'},
+            commitHash: 'faefgae',
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository (null)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: null,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            account: {username: '2552'},
+            repository: null,
+            commitHash: 'faefgae',
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository.name (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Record<keyof Pick<Repository, 'name'>, any>,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: true},
+            commitHash: 'faefgae',
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with commitHash (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: any,
+            directoryPath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: 1111,
+            directoryPath: 'faefaef',
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with directoryPath (string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            directoryPath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+            directoryPath: 'fagfaegaeg',
+        };
+        expect(directory(fakeBody)).toBeTrue();
+    });
+
+    it('should handle body with directoryPath (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            directoryPath: any,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+            directoryPath: 255235,
+        };
+        expect(directory(fakeBody)).toBeFalse();
+    });
+});
+
+describe('commitCount', () =>
+{
+    it('should handle body without account', function ()
+    {
+        const fakeBody: {
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+        } = {
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account (null)', function ()
+    {
+        const fakeBody: {
+            account: null,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+        } = {
+            account: null,
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account.username (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Record<keyof Pick<Account, 'username'>, any>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+        } = {
+            account: {username: 2552},
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body without repository', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            commitHash: string,
+        } = {
+            account: {username: '2552'},
+            commitHash: 'faefgae',
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository (null)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: null,
+            commitHash: string,
+        } = {
+            account: {username: '2552'},
+            repository: null,
+            commitHash: 'faefgae',
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository.name (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Record<keyof Pick<Repository, 'name'>, any>,
+            commitHash: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: true},
+            commitHash: 'faefgae',
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with commitHash (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: any,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: 1111,
+        };
+        expect(commitCount(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+        };
+        expect(commitCount(fakeBody)).toBeTrue();
+    });
+
+});
+
+describe('fileInfo', () =>
+{
+    it('should handle body without account', function ()
+    {
+        const fakeBody: {
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account (null)', function ()
+    {
+        const fakeBody: {
+            account: null,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: null,
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account.username (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Record<keyof Pick<Account, 'username'>, any>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: 2552},
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body without repository', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository (null)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: null,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: null,
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository.name (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Record<keyof Pick<Repository, 'name'>, any>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: true},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with commitHash (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: any,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: 1111,
+            filePath: 'faefaef',
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with filePath (string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+            filePath: 'fagfaegaeg',
+        };
+        expect(fileInfo(fakeBody)).toBeTrue();
+    });
+
+    it('should handle body with filePath (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: any,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+            filePath: 255235,
+        };
+        expect(fileInfo(fakeBody)).toBeFalse();
+    });
+});
+
+describe('rawFile', () =>
+{
+    it('should handle body without account', function ()
+    {
+        const fakeBody: {
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account (null)', function ()
+    {
+        const fakeBody: {
+            account: null,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: null,
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with account.username (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Record<keyof Pick<Account, 'username'>, any>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: 2552},
+            repository: {name: 'faefgaeg'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body without repository', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository (null)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: null,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: null,
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with repository.name (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Record<keyof Pick<Repository, 'name'>, any>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: true},
+            commitHash: 'faefgae',
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with commitHash (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: any,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: 1111,
+            filePath: 'faefaef',
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
+    });
+
+    it('should handle body with filePath (string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: string,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+            filePath: 'fagfaegaeg',
+        };
+        expect(rawFile(fakeBody)).toBeTrue();
+    });
+
+    it('should handle body with filePath (not a string)', function ()
+    {
+        const fakeBody: {
+            account: Pick<Account, 'username'>,
+            repository: Pick<Repository, 'name'>,
+            commitHash: string,
+            filePath: any,
+        } = {
+            account: {username: '2552'},
+            repository: {name: 'faegfaeg'},
+            commitHash: '1111',
+            filePath: 255235,
+        };
+        expect(rawFile(fakeBody)).toBeFalse();
     });
 });
