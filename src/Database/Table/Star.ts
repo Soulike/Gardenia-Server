03ac76912030ad9e959cede4109f39f50c545f04
Soulike <316413310@qq.com>
2020-02-03 17:02:25 +0800

    数据库添加接口
    - 数据库 Star 表添加新接口 insert、del、select、count 及对应测试

---
+++ src/Database/Table/Star.ts
@@ -0,0 +1,61 @@
+import {Star} from '../../Class';
+import {
+    executeTransaction,
+    generateColumnNamesAndValuesArrayAndParameterString,
+    generateParameterizedStatementAndValuesArray,
+} from '../Function';
+import pool from '../Pool';
+
+export async function insert(star: Readonly<Star>): Promise<void>
+{
+    const client = await pool.connect();
+    try
+    {
+        await executeTransaction(client, async client =>
+        {
+            const {values, columnNames, parameterString} = generateColumnNamesAndValuesArrayAndParameterString(star);
+            await client.query(`INSERT INTO stars (${columnNames}) VALUES (${parameterString})`, values);
+        });
+    }
+    finally
+    {
+        client.release();
+    }
+}
+
+export async function del(star: Readonly<Star>): Promise<void>
+{
+    const client = await pool.connect();
+    try
+    {
+        const {username, repository_username, repository_name} = star;
+        await client.query(`DELETE
+                            FROM stars
+                            WHERE username = $1
+                              AND repository_username = $2
+                              AND repository_name = $3`,
+            [username, repository_username, repository_name]);
+    }
+    finally
+    {
+        client.release();
+    }
+}
+
+export async function select(star: Readonly<Partial<Star>>): Promise<Star[]>
+{
+    const {parameterizedStatement, values} = generateParameterizedStatementAndValuesArray(star, 'AND');
+    const {rows} = await pool.query(
+        `SELECT * FROM stars WHERE ${parameterizedStatement}`,
+        values);
+    return rows.map(row => Star.from(row));
+}
+
+export async function count(star: Readonly<Partial<Star>>): Promise<number>
+{
+    const {parameterizedStatement, values} = generateParameterizedStatementAndValuesArray(star, 'AND');
+    const {rows} = await pool.query(
+        `SELECT COUNT(*) as count FROM stars WHERE ${parameterizedStatement}`,
+        values);
+    return Number.parseInt(rows[0]['count']);
+}
