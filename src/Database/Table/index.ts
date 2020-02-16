276bf7f906316a9dd233b66b39549f21b2a41ccf
Soulike <316413310@qq.com>
2020-02-16 16:18:42 +0800

    数据库添加新表
    - 数据库添加表 forks
    - 添加新数据库类 RepositoryRepository
    - 数据库模块添加 Fork 模块

--- src/Database/Table/index.ts
+++ src/Database/Table/index.ts
@@ -4,5 +4,6 @@ import * as Repository from './Repository';
 import * as Group from './Group';
 import * as Star from './Star';
 import * as Collaborate from './Collaborate';
+import * as Fork from './Fork';
 
-export {Account, Profile, Repository, Group, Star, Collaborate};
+export {Account, Profile, Repository, Group, Star, Collaborate, Fork};
