9bbbd9f747ac2d98f39c5331a75ea4856278a14e
Soulike <316413310@qq.com>
2020-03-15 18:40:48 +0800

    添加配置文件 MAIL 注释

--- src/CONFIG/MAIL.ts
+++ src/CONFIG/MAIL.ts
@@ -6,7 +6,7 @@ export const MAIL = Object.freeze({
     host: '',
     secure: true,
     auth: {
-        user: '',
+        user: '',  // 这里必须填写完整的邮件地址，否则可能会导致发不出去邮件
         pass: '',
     },
 });
