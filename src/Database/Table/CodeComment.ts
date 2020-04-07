1e19fdc045229330f5802f841490f466d8672125
Soulike <316413310@qq.com>
2020-04-07 22:10:06 +0800

    完成 CodeComment 模块服务层，补充缺失的数据库层接口

--- src/Database/Table/CodeComment.ts
+++ src/Database/Table/CodeComment.ts
@@ -58,6 +58,19 @@ export async function selectByRepositoryAndFilePath(codeComment: Readonly<Pick<C
     return rows.map(row => CodeComment.from(row));
 }
 
+export async function selectById(codeComment: Readonly<Pick<CodeComment, 'id'>>): Promise<CodeComment | null>
+{
+    const {id} = codeComment;
+    const {rows} = await pool.query(`SELECT *
+                                     FROM code_comments
+                                     WHERE id = $1`, [id]);
+    if (rows.length !== 1)
+    {
+        return null;
+    }
+    return CodeComment.from(rows[0]);
+}
+
 export async function update(codeComment: Readonly<Partial<Omit<CodeComment, 'id'>>>, primaryKey: Readonly<Pick<CodeComment, 'id'>>): Promise<void>
 {
     if (Object.keys(codeComment).length !== 0)
---
+++ src/Service/CodeComment.ts
@@ -0,0 +1,103 @@
+import {Account, CodeComment, ResponseBody, ServiceResponse} from '../Class';
+import {
+    CodeComment as CodeCommentTable,
+    Collaborate as CollaborateTable,
+    Repository as RepositoryTable,
+} from '../Database';
+import {Repository as RepositoryFunction} from '../Function';
+
+export async function add(codeComment: Readonly<Pick<CodeComment, 'repositoryUsername' | 'repositoryName' | 'filePath' | 'columnNumber' | 'content' | 'creationCommitHash'>>, usernameInSession: Account['username']): Promise<ServiceResponse<void>>
+{
+    const {repositoryUsername, repositoryName, filePath, creationCommitHash, content, columnNumber} = codeComment;
+    const repository = await RepositoryTable.selectByUsernameAndName({
+        username: repositoryUsername,
+        name: repositoryName,
+    });
+    if (repository === null || !await RepositoryFunction.repositoryIsAvailableToTheViewer(repository, {username: usernameInSession}))
+    {
+        return new ServiceResponse<void>(404, {},
+            new ResponseBody(false, `仓库 ${repositoryUsername}/${repositoryName} 不存在`));
+    }
+    const timestamp = Date.now();
+    await CodeCommentTable.insert({
+        repositoryUsername,
+        repositoryName,
+        filePath,
+        creationCommitHash,
+        content,
+        columnNumber,
+        creatorUsername: usernameInSession,
+        modificationTimestamp: timestamp,
+        creationTimestamp: timestamp,
+    });
+    return new ServiceResponse<void>(200, {},
+        new ResponseBody(true));
+}
+
+export async function del(codeComment: Readonly<Pick<CodeComment, 'id'>>, usernameInSession: Account['username']): Promise<ServiceResponse<void>>
+{
+    const {id} = codeComment;
+    const codeCommentInDatabase = await CodeCommentTable.selectById({id});
+    if (codeCommentInDatabase === null)
+    {
+        return new ServiceResponse<void>(404, {},
+            new ResponseBody(false, '代码批注不存在'));
+    }
+    const {repositoryUsername, repositoryName, creatorUsername} = codeCommentInDatabase;
+    // repository 一定存在
+    const accountRepositories = await CollaborateTable.select({repositoryUsername, repositoryName});
+    const collaborators = accountRepositories.map(({username}) => username);
+    if (usernameInSession !== repositoryUsername        // 不是仓库创建者
+        && !collaborators.includes(usernameInSession)   // 不是仓库合作者
+        && usernameInSession !== creatorUsername)       // 不是批注创建者
+    {
+        return new ServiceResponse<void>(200, {},
+            new ResponseBody(false, '只有仓库的合作者或代码批注的创建者可以删除代码批注'));
+    }
+    await CodeCommentTable.del({id});
+    return new ServiceResponse<void>(200, {},
+        new ResponseBody(true));
+}
+
+export async function get(codeComment: Readonly<Pick<CodeComment, 'repositoryUsername' | 'repositoryName' | 'filePath'>>, usernameInSession?: Account['username']): Promise<ServiceResponse<{ codeComments: CodeComment[] } | void>>
+{
+    const {repositoryUsername, repositoryName, filePath} = codeComment;
+    const repository = await RepositoryTable.selectByUsernameAndName({
+        username: repositoryUsername,
+        name: repositoryName,
+    });
+    if (repository === null || !await RepositoryFunction.repositoryIsAvailableToTheViewer(repository, {username: usernameInSession}))
+    {
+        return new ServiceResponse<void>(404, {},
+            new ResponseBody(false, `仓库 ${repositoryUsername}/${repositoryName} 不存在`));
+    }
+    const codeComments = await CodeCommentTable.selectByRepositoryAndFilePath({
+        repositoryUsername,
+        repositoryName,
+        filePath,
+    });
+    return new ServiceResponse(200, {},
+        new ResponseBody(true, '', {codeComments}));
+}
+
+export async function update(codeComment: Readonly<Pick<CodeComment, 'content'>>, primaryKey: Readonly<Pick<CodeComment, 'id'>>, usernameInSession: Account['username']): Promise<ServiceResponse<void>>
+{
+    const {content} = codeComment;
+    const {id} = primaryKey;
+    const codeCommentInDatabase = await CodeCommentTable.selectById({id});
+    if (codeCommentInDatabase === null)
+    {
+        return new ServiceResponse<void>(404, {},
+            new ResponseBody(false, '代码批注不存在'));
+    }
+    const {creatorUsername} = codeCommentInDatabase;
+    if (creatorUsername !== usernameInSession)
+    {
+        return new ServiceResponse<void>(200, {},
+            new ResponseBody(false, '只有代码批注创建者可以修改此批注'));
+    }
+    const now = Date.now();
+    await CodeCommentTable.update({content, modificationTimestamp: now}, {id});
+    return new ServiceResponse<void>(200, {},
+        new ResponseBody(true));
+}
