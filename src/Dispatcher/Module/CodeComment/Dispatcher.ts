ecf9dd65b66a1ff608a43263eba38197e644c03d
Soulike <316413310@qq.com>
2020-06-15 11:01:45 +0800

    修改 CodeComment 模块参数验证器为中间件形式

--- src/Dispatcher/Module/CodeComment/Dispatcher.ts
+++ src/Dispatcher/Module/CodeComment/Dispatcher.ts
@@ -5,12 +5,13 @@ import bodyParser from '../../Middleware/bodyParser';
 import {add, del, get, update} from './Middleware';
 import JSONQuerystringParser from '../../Middleware/JSONQuerystringParser';
 import sessionChecker from '../../Middleware/sessionChecker';
+import * as ParameterValidator from './ParameterValidator';
 
 export default (router: Router<IState, IContext>) =>
 {
     router
-        .post(ADD, sessionChecker(), bodyParser(), add())
-        .post(DEL, sessionChecker(), bodyParser(), del())
-        .get(GET, JSONQuerystringParser(), get())
-        .post(UPDATE, sessionChecker(), bodyParser(), update());
+        .post(ADD, sessionChecker(), bodyParser(), ParameterValidator.add(), add())
+        .post(DEL, sessionChecker(), bodyParser(), ParameterValidator.del(), del())
+        .get(GET, JSONQuerystringParser(), ParameterValidator.get(), get())
+        .post(UPDATE, sessionChecker(), bodyParser(), ParameterValidator.update(), update());
 }
--- src/Dispatcher/Module/CodeComment/Middleware.ts
+++ src/Dispatcher/Module/CodeComment/Middleware.ts
@@ -1,16 +1,10 @@
 import {IRouteHandler} from '../../Interface';
-import {WrongParameterError} from '../../Class';
-import * as ParameterValidator from './ParameterValidator';
 import {CodeComment as CodeCommentService} from '../../../Service';
 
 export const add: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.add(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {
             repositoryUsername, repositoryName,
             filePath, columnNumber, content, creationCommitHash,
@@ -26,10 +20,6 @@ export const del: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.del(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {id} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await CodeCommentService.del({id}, username!);
@@ -40,10 +30,6 @@ export const get: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.get(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {codeComment, commitHash} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await CodeCommentService.get(codeComment, commitHash, username);
@@ -54,10 +40,6 @@ export const update: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.update(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {codeComment, primaryKey} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await CodeCommentService.update(codeComment, primaryKey, username!);
