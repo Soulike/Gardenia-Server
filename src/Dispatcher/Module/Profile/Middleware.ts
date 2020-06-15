9777b669f4b905d59beda2acd5aae30c636dbd5e
Soulike <316413310@qq.com>
2020-06-15 14:49:24 +0800

    修改 Profile 模块参数验证器为中间件形式

--- src/Dispatcher/Module/Profile/Middleware.ts
+++ src/Dispatcher/Module/Profile/Middleware.ts
@@ -1,16 +1,10 @@
 import {Profile} from '../../../Service';
 import {IRouteHandler} from '../../Interface';
-import * as ParameterValidator from './ParameterValidator';
-import {WrongParameterError} from '../../Class';
 
 export const get: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.get(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await Profile.get(username, account);
@@ -21,10 +15,6 @@ export const getByEmail: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.getByEmail(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {email} = ctx.request.body;
         ctx.state.serviceResponse = await Profile.getByEmail(email);
     };
@@ -34,10 +24,6 @@ export const setNickname: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.setNickname(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {nickname} = ctx.request.body;
         const {username: usernameInSession} = ctx.session;
         ctx.state.serviceResponse = await Profile.setNickname(nickname, usernameInSession!);
@@ -48,10 +34,6 @@ export const setEmail: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.setEmail(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {email, verificationCode} = ctx.request.body;
         const {username, verification} = ctx.session;
         ctx.state.serviceResponse = await Profile.setEmail(email, verificationCode, username!, verification);
@@ -62,10 +44,6 @@ export const sendSetEmailVerificationCodeToEmail: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.sendSetEmailVerificationCodeToEmail(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {email} = ctx.request.body;
         ctx.state.serviceResponse = await Profile.sendSetEmailVerificationCodeToEmail(email);
     };
@@ -76,11 +54,7 @@ export const uploadAvatar: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (typeof ctx.request.files === 'undefined' || !ParameterValidator.uploadAvatar(ctx.request.files))
-        {
-            throw new WrongParameterError();
-        }
-        const {avatar} = ctx.request.files;
+        const {avatar} = ctx.request.files!;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await Profile.uploadAvatar(avatar, username!);
     };
