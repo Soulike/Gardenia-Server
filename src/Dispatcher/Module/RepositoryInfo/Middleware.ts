05c92b63e3291dd5dfbd17016a38d3f392263655
Soulike <316413310@qq.com>
2020-06-15 17:19:06 +0800

    修改 RepositoryInfo 模块参数验证器为中间件形式

--- src/Dispatcher/Module/RepositoryInfo/Middleware.ts
+++ src/Dispatcher/Module/RepositoryInfo/Middleware.ts
@@ -1,16 +1,10 @@
 import {IRouteHandler} from '../../Interface';
 import {RepositoryInfo} from '../../../Service';
-import * as ParameterValidator from './ParameterValidator';
-import {WrongParameterError} from '../../Class';
 
 export const repository: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.repository(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account, repository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.repository(account, repository, username);
@@ -21,10 +15,7 @@ export const branches: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.branches(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
+
         const {repository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.branches(repository, username);
@@ -35,10 +26,6 @@ export const branchNames: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.branchNames(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {repository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.branchNames(repository, username);
@@ -49,10 +36,6 @@ export const lastBranchCommit: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.lastBranchCommit(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account, repository, branch, filePath} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.lastBranchCommit(account, repository, branch, filePath, username);
@@ -63,10 +46,6 @@ export const lastCommit: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.lastCommit(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {repository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.lastCommit(repository, username);
@@ -77,10 +56,6 @@ export const directory: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.directory(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account, repository, commitHash, directoryPath} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.directory(account, repository, commitHash, directoryPath, username);
@@ -91,10 +66,6 @@ export const commitCount: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.commitCount(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account, repository, commitHash} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.commitCount(account, repository, commitHash, username);
@@ -105,10 +76,6 @@ export const commitCountBetweenCommits: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.commitCount(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {repository, baseCommitHash, targetCommitHash} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.commitCountBetweenCommits(repository, baseCommitHash, targetCommitHash, username);
@@ -119,10 +86,6 @@ export const fileInfo: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.fileInfo(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account, repository, filePath, commitHash} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.fileInfo(account, repository, filePath, commitHash, username);
@@ -133,10 +96,6 @@ export const rawFile: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.rawFile(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {account, repository, filePath, commitHash} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.rawFile(account, repository, filePath, commitHash, username);
@@ -147,10 +106,6 @@ export const setName: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.setName(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {repository, newRepository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.setName(repository, newRepository, username!);
@@ -161,10 +116,6 @@ export const setDescription: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.setDescription(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {repository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.setDescription(repository, username!);
@@ -175,10 +126,6 @@ export const setIsPublic: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.setIsPublic(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {repository} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryInfo.setIsPublic(repository, username!);
@@ -189,10 +136,6 @@ export const commitHistoryBetweenCommits: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.commitHistoryBetweenCommits(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, baseCommitHash, targetCommitHash, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.commitHistoryBetweenCommits(repository, baseCommitHash, targetCommitHash, offset, limit, username);
@@ -203,10 +146,6 @@ export const commitHistory: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.commitHistory(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, targetCommitHash, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.commitHistory(repository, targetCommitHash, offset, limit, username);
@@ -217,10 +156,6 @@ export const fileCommitHistoryBetweenCommits: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.fileCommitHistoryBetweenCommits(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, filePath, baseCommitHash, targetCommitHash, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.fileCommitHistoryBetweenCommits(repository, filePath, baseCommitHash, targetCommitHash, offset, limit, username);
@@ -231,10 +166,6 @@ export const fileCommitHistory: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.fileCommitHistory(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, filePath, targetCommitHash, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.fileCommitHistory(repository, filePath, targetCommitHash, offset, limit, username);
@@ -245,10 +176,6 @@ export const diffBetweenCommits: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.diffBetweenCommits(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, baseCommitHash, targetCommitHash, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.diffBetweenCommits(repository, baseCommitHash, targetCommitHash, offset, limit, username);
@@ -259,10 +186,6 @@ export const diffAmountBetweenCommits: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.diffAmountBetweenCommits(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, baseCommitHash, targetCommitHash} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.diffAmountBetweenCommits(repository, baseCommitHash, targetCommitHash, username);
@@ -273,10 +196,6 @@ export const fileDiffBetweenCommits: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.fileDiffBetweenCommits(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, filePath, baseCommitHash, targetCommitHash} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.fileDiffBetweenCommits(repository, filePath, baseCommitHash, targetCommitHash, username);
@@ -287,10 +206,6 @@ export const commit: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.commit(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, commitHash} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.commit(repository, commitHash, username);
@@ -301,10 +216,6 @@ export const commitDiff: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.commitDiff(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, commitHash, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.commitDiff(repository, commitHash, offset, limit, username);
@@ -315,10 +226,6 @@ export const commitDiffAmount: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.commitDiffAmount(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, commitHash} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.commitDiffAmount(repository, commitHash, username);
@@ -329,10 +236,6 @@ export const fileCommit: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.fileCommit(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username} = ctx.session;
         const {repository, filePath, commitHash} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.fileCommit(repository, filePath, commitHash, username);
@@ -343,10 +246,6 @@ export const forkAmount: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkAmount(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {username, name} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkAmount({username, name}, usernameInSession);
@@ -357,10 +256,6 @@ export const forkRepositories: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkRepositories(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {username, name} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkRepositories({username, name}, usernameInSession);
@@ -371,10 +266,6 @@ export const forkFrom: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkFrom(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {username, name} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkFrom({username, name}, usernameInSession);
@@ -385,10 +276,6 @@ export const forkCommitHistory: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkCommitHistory(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkCommitHistory(sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch, offset, limit, usernameInSession);
@@ -399,10 +286,6 @@ export const forkCommitAmount: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkCommitAmount(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkCommitAmount(sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch, usernameInSession);
@@ -413,10 +296,6 @@ export const forkFileDiff: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkFileDiff(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch, offset, limit} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkFileDiff(sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch, offset, limit, usernameInSession);
@@ -427,10 +306,6 @@ export const forkFileDiffAmount: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.forkFileDiffAmount(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.forkFileDiffAmount(sourceRepository, sourceRepositoryBranch, targetRepository, targetRepositoryBranch, usernameInSession);
@@ -441,10 +316,6 @@ export const hasCommonAncestor: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.hasCommonAncestor(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {sourceRepository, sourceRepositoryBranchName, targetRepository, targetRepositoryBranchName} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryInfo.hasCommonAncestor(sourceRepository, sourceRepositoryBranchName, targetRepository, targetRepositoryBranchName, usernameInSession);
