0345885d2592c5012905a842d14631a445bdfecd
Soulike <316413310@qq.com>
2020-06-15 15:20:58 +0800

    修改 Repository 模块参数验证器为中间件形式

--- src/Dispatcher/Module/Repository/Dispatcher.ts
+++ src/Dispatcher/Module/Repository/Dispatcher.ts
@@ -4,12 +4,13 @@ import {create, del, fork, getRepositories, isMergeable} from './Middleware';
 import bodyParser from '../../Middleware/bodyParser';
 import {IDispatcher} from '../../Interface';
 import sessionChecker from '../../Middleware/sessionChecker';
+import * as ParameterValidator from './ParameterValidator';
 
 export default (router =>
 {
-    router.post(CREATE, sessionChecker(), bodyParser(), create())
-        .post(DEL, sessionChecker(), bodyParser(), del())
-        .get(GET_REPOSITORIES, JSONQuerystringParser(), getRepositories())
-        .post(FORK, sessionChecker(), bodyParser(), fork())
-        .get(IS_MERGEABLE, JSONQuerystringParser(), isMergeable());
+    router.post(CREATE, sessionChecker(), bodyParser(), ParameterValidator.create(), create())
+        .post(DEL, sessionChecker(), bodyParser(), ParameterValidator.del(), del())
+        .get(GET_REPOSITORIES, JSONQuerystringParser(), ParameterValidator.getRepositories(), getRepositories())
+        .post(FORK, sessionChecker(), bodyParser(), ParameterValidator.fork(), fork())
+        .get(IS_MERGEABLE, JSONQuerystringParser(), ParameterValidator.isMergeable(), isMergeable());
 }) as IDispatcher;
--- src/Dispatcher/Module/Repository/Middleware.ts
+++ src/Dispatcher/Module/Repository/Middleware.ts
@@ -1,16 +1,10 @@
 import {IRouteHandler} from '../../Interface';
 import {Repository as RepositoryService} from '../../../Service';
-import * as ParameterValidator from './ParameterValidator';
-import {WrongParameterError} from '../../Class';
 
 export const create: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.create(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {name, description, isPublic} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryService.create({name, description, isPublic}, username!);
@@ -21,10 +15,6 @@ export const del: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.del(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {name} = ctx.request.body;
         const {username} = ctx.session;
         ctx.state.serviceResponse = await RepositoryService.del({name}, username!);
@@ -35,10 +25,6 @@ export const getRepositories: IRouteHandler = () =>
 {
     return async (ctx) =>
     {
-        if (!ParameterValidator.getRepositories(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {start, end, username} = ctx.request.body;
         const {username: usernameInSession} = ctx.session;
         ctx.state.serviceResponse = await RepositoryService.getRepositories(start, end, username, usernameInSession);
@@ -50,10 +36,6 @@ export const fork: IRouteHandler = () =>
     return async ctx =>
     {
         const {username: usernameInSession} = ctx.session;
-        if (!ParameterValidator.fork(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username, name} = ctx.request.body;
         ctx.state.serviceResponse = await RepositoryService.fork({username, name}, usernameInSession!);
     };
@@ -63,10 +45,6 @@ export const isMergeable: IRouteHandler = () =>
 {
     return async ctx =>
     {
-        if (!ParameterValidator.isMergeable(ctx.request.body))
-        {
-            throw new WrongParameterError();
-        }
         const {username: usernameInSession} = ctx.session;
         const {
             sourceRepository,
