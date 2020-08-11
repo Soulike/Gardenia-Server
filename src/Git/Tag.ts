dd49253bd640117a225709774de6c7dd5be7d98c
Soulike <316413310@qq.com>
2020-08-11 13:40:02 +0800

    补充 Tag 类遗漏的信息

--- src/Git/Tag.ts
+++ src/Git/Tag.ts
@@ -11,7 +11,13 @@ export async function getTagNames(repositoryPath: string): Promise<string[]>
 
 export async function getTagInfo(repositoryPath: string, tagName: string): Promise<Tag>
 {
-    return new Tag(tagName, await getCommit(repositoryPath, tagName));
+    const subject = (await Promisify.execPromise(
+        `git tag -l --format='%(contents:subject)' '${tagName}'`,
+        {cwd: repositoryPath})).trim();
+    const body = (await Promisify.execPromise(
+        `git tag -l --format='%(contents:body)' '${tagName}'`,
+        {cwd: repositoryPath})).trim();
+    return new Tag(tagName, {subject, body}, await getCommit(repositoryPath, tagName));
 }
 
 export async function getTagsInfo(repositoryPath: string, offset: number = 0, limit: number = Number.MAX_SAFE_INTEGER): Promise<Tag[]>
